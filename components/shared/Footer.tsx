038da3489378ab29aefd06b3fb8cb4e4c2c6bcb1
Kanisevsky <meltsmokestudio@gmail.com>
2024-02-29 23:12:20 +0000

    footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
@@ -5,7 +6,14 @@ const Footer = () => {
   return (
     <footer className="border-t">
       <div className="">
-        <Link href="/"></Link>
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={128}
+            height={38}
+          />
+        </Link>
       </div>
     </footer>
   );
