e514ed206819d68c7589d8048e2c86fef5650ab1
Kanisevsky <meltsmokestudio@gmail.com>
2024-02-29 23:11:07 +0000

    footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,14 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t">
+      <div className="">
+        <Link href="/"></Link>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
