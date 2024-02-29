872d36d5f9c60a6cc52101598d79c6aec626830a
Kanisevsky <meltsmokestudio@gmail.com>
2024-02-29 23:07:17 +0000

    mobile nav

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+'use client';
+import { headerLinks } from '@/constants';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const NavItems = () => {
-  return <ul className="md:flex-betwen flex">{}</ul>;
+  const pathname = usePathname();
+  return (
+    <ul className="md:flex-betwen flex w-full flex-col items-start gap-5 md:flex-row">
+      {headerLinks.map((link) => {
+        const isActive = pathname === link.route;
+        return (
+          <li
+            key={link.route}
+            className={`${
+              isActive && 'text-primary-500'
+            } flex-center p-medium-16 whitespace-nowrap`}
+          >
+            <Link href={link.route}>{link.label}</Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default NavItems;
