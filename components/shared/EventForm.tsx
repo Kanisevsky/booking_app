e2bfbace1da3bd48ed5a20be0c45a53fed3890a9
Kanisevsky <meltsmokestudio@gmail.com>
2024-04-18 21:17:59 +0100

    Default shadcn form creation

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -11,6 +11,8 @@ import {
   FormLabel,
   FormMessage,
 } from '@/components/ui/form';
+import { Input } from '../ui/input';
+import { Button } from '../ui/button';
 
 type EventFormProps = {
   userId: string;
@@ -39,7 +41,29 @@ const EventForm = ({ userId, type }: EventFormProps) => {
     console.log(values);
   }
 
-  return <div>eventForm</div>;
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 };
 
 export default EventForm;
