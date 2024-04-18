232932636b29fc1282926bb13f3527b1c5811693
Kanisevsky <meltsmokestudio@gmail.com>
2024-04-18 21:27:00 +0100

    Zod Form Validation

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -2,6 +2,7 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
+import { eventFormSchema } from '@/lib/validator';
 import {
   Form,
   FormControl,
@@ -19,23 +20,17 @@ type EventFormProps = {
   type: 'Create' | 'Update';
 };
 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: 'Username must be at least 2 characters.',
-  }),
-});
-
 const EventForm = ({ userId, type }: EventFormProps) => {
   // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof eventFormSchema>>({
+    resolver: zodResolver(eventFormSchema),
     defaultValues: {
-      username: '',
+      title: '',
     },
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  function onSubmit(values: z.infer<typeof eventFormSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values);
