8554cf3abf03a0b2de72b6273946c86f775e63a9
Kanisevsky <meltsmokestudio@gmail.com>
2024-04-18 18:37:50 +0100

    User CRUD actions implemented

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,9 +1,10 @@
 'use server';
 
-import { CreateUserParams } from '@/types';
+import { CreateUserParams, UpdateUserParams } from '@/types';
 import { handleError } from '../utils';
 import { connectToDatabase } from '../database';
 import User from '../database/models/user.model';
+import { revalidatePath } from 'next/cache';
 
 export const createUser = async (user: CreateUserParams) => {
   try {
@@ -15,3 +16,39 @@ export const createUser = async (user: CreateUserParams) => {
     handleError(error);
   }
 };
+
+export async function updateUser(clerkId: string, user: UpdateUserParams) {
+  try {
+    await connectToDatabase();
+
+    const updatedUser = await User.findOneAndUpdate({ clerkId }, user, {
+      new: true,
+    });
+
+    if (!updatedUser) throw new Error('User update failed');
+    return JSON.parse(JSON.stringify(updatedUser));
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+export async function deleteUser(clerkId: string) {
+  try {
+    await connectToDatabase();
+
+    // Find user to delete
+    const userToDelete = await User.findOne({ clerkId });
+
+    if (!userToDelete) {
+      throw new Error('User not found');
+    }
+
+    // Delete user
+    const deletedUser = await User.findByIdAndDelete(userToDelete._id);
+    revalidatePath('/');
+
+    return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null;
+  } catch (error) {
+    handleError(error);
+  }
+}
