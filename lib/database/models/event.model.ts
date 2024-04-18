3b989afeac39e31a85fd463488e1b816f23c7264
Kanisevsky <meltsmokestudio@gmail.com>
2024-04-18 10:39:58 +0100

    Mongodb Schema models

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -1,9 +1,25 @@
 import { Schema, model, models } from 'mongoose';
 
+export interface IEvent extends Document {
+  _id: string;
+  title: string;
+  description?: string;
+  city?: string;
+  createdAt: Date;
+  imageUrl: string;
+  startDateTime: Date;
+  endDateTime: Date;
+  price?: string;
+  isFree: boolean;
+  url?: string;
+  category: { _id: string; name: string };
+  organizer: { _id: string; firstName: string; lastName: string };
+}
+
 const EventSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String },
-  location: { type: String },
+  city: { type: String },
   createdAt: { type: Date, default: Date.now },
   imageUrl: { type: String, required: true },
   startDateTime: { type: Date, default: Date.now },
