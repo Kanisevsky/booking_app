b0cce914ccca66a6eb17138454a5097f46b958a2
Kanisevsky <meltsmokestudio@gmail.com>
2024-04-18 21:04:57 +0100

    create an event page

---
+++ components/shared/EventForm.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+type EventFormProps = {
+  userId: string;
+  type: 'Create' | 'Update';
+};
+
+const EventForm = ({ userId, type }: EventFormProps) => {
+  return <div>eventForm</div>;
+};
+
+export default EventForm;
