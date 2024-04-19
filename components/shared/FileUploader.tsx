4515bae2de7e9b0f0c55e731bb5df054bf95c3f4
Kanisevsky <meltsmokestudio@gmail.com>
2024-04-19 22:08:04 +0100

    file upload in the form

--- components/shared/FileUploader.tsx
+++ components/shared/FileUploader.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import { Dispatch, SetStateAction } from "react";
 
-const FileUploader = () => {
+type FileUploadProps = {
+    imageUrl:string,
+    onFieldChange: (value: string) => void,
+    setFiles:Dispatch<SetStateAction<File[]>>
+}
+
+
+const FileUploader = ({ setFiles, imageUrl, onFieldChange }:<FileUploadProps> ) => {
   return <div>FileUploader</div>;
 };
 
