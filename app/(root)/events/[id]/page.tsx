eee022205e19c2341c8dae9b4bd62177cf7d030f
Kanisevsky <meltsmokestudio@gmail.com>
2024-04-24 20:14:45 +0100

    event details page

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { getEventById } from '@/lib/actions/event.actions';
+import { formatDateTime } from '@/lib/utils';
 import { SearchParamProps } from '@/types';
 import Image from 'next/image';
 
@@ -45,7 +46,25 @@ const EventDetails = async ({ params: { id } }: SearchParamProps) => {
                 width={32}
                 height={32}
               />
-              <div className="p-medium-16 lg:p-regular-20 flex flex-wrap items-center"></div>
+              <div className="p-medium-16 lg:p-regular-20 flex flex-wrap items-center">
+                <p>
+                  {formatDateTime(event.startDateTime).dateOnly} -{' '}
+                  {formatDateTime(event.startDateTime).timeOnly}
+                </p>
+                <p>
+                  {formatDateTime(event.endDateTime).dateOnly} -{' '}
+                  {formatDateTime(event.endDateTime).timeOnly}
+                </p>
+              </div>
+            </div>
+            <div className="p-regular-20 flex items-center gap-3">
+              <Image
+                src="/assets/icons/location.svg"
+                alt="location"
+                width={32}
+                height={32}
+              />
+              <p className="p-medium-16 lg:p-regular-20">{event.location}</p>
             </div>
           </div>
         </div>
