a741847634146c08774390e5e5d8087e63b16ef2
Kanisevsky <meltsmokestudio@gmail.com>
2024-04-29 19:34:35 +0100

    collections

---
+++ components/shared/Pagination.tsx
@@ -0,0 +1,11 @@
+type PaginationProps = {
+  page: number | string;
+  totalPages: number;
+  urlParamName?: string;
+};
+
+const Pagination = ({ urlParamName, page, totalPages }: PaginationProps) => {
+  return <div>Pagination</div>;
+};
+
+export default Pagination;
