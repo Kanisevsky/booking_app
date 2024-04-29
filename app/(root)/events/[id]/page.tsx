61f7a41e429356c8150f595fe99436947c743373
Kanisevsky <meltsmokestudio@gmail.com>
2024-04-29 18:53:37 +0100

    event details page

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -9,7 +9,7 @@ const EventDetails = async ({ params: { id } }: SearchParamProps) => {
 
   return (
     <section className="flex justify-center bg-primary-50 bg-dotted-pattern bg-contain">
-      <div className="grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl">
+      <div className="grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl p-12 xl:p-24">
         <Image
           src={event.imageUrl}
           alt="hero Image"
