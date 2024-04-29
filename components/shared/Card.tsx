287143980172e99ac0ee554cdb119d5616f8b405
Kanisevsky <meltsmokestudio@gmail.com>
2024-04-29 19:48:30 +0100

    Card

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import DeleteConfirmation from './DeleteConfirmation';
+import { formatDateTime } from '@/lib/utils';
 
 type CardProps = {
   event: IEvent;
@@ -35,6 +36,50 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
           </Link>
 
           <DeleteConfirmation eventId={event._id} />
+
+          <div className="flex min-h-[230px] flex-col gap-3 p-5 md:gap-4">
+            {!hidePrice && (
+              <div className="flex gap-2">
+                <span className="p-semibold-14 w-min rounded-full bg-green-100 px-4 py-1 text-green-60">
+                  {event.isFree ? 'FREE' : `$${event.price}`}
+                </span>
+                <p className="p-semibold-14 w-min rounded-full bg-grey-500/10 px-4 py-1 text-grey-500 line-clamp-1">
+                  {event.category.name}
+                </p>
+              </div>
+            )}
+
+            <p className="p-medium-16 p-medium-18 text-grey-500">
+              {formatDateTime(event.startDateTime).dateTime}
+            </p>
+
+            <Link href={`/events/${event._id}`}>
+              <p className="p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black">
+                {event.title}
+              </p>
+            </Link>
+
+            <div className="flex-between w-full">
+              <p className="p-medium-14 md:p-medium-16 text-grey-600">
+                {event.organizer.firstName} {event.organizer.lastName}
+              </p>
+
+              {hasOrderLink && (
+                <Link
+                  href={`/orders?eventId=${event._id}`}
+                  className="flex gap-2"
+                >
+                  <p className="text-primary-500">Order Details</p>
+                  <Image
+                    src="/assets/icons/arrow.svg"
+                    alt="search"
+                    width={10}
+                    height={10}
+                  />
+                </Link>
+              )}
+            </div>
+          </div>
         </div>
       )}
     </div>
